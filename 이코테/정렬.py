67842b6e7a08030303c01285d3069ea66ae1c7a1
amy3374 <119571479+amy3374@users.noreply.github.com>
2023-04-18 23:54:02 +0900

    first

--- 이코테/정렬.py
+++ 이코테/정렬.py
@@ -1,22 +1,34 @@
 # 선택정렬
-array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+# array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
-for i in range(len(array)):
-    min_index = i
-    for j in range(i+1, len(array)):
-        if array[min_index] > array[j]:
-            min_index = j
-    array[i], array[min_index] = array[min_index], array[i]
+# for i in range(len(array)):
+#     min_index = i
+#     for j in range(i+1, len(array)):
+#         if array[min_index] > array[j]:
+#             min_index = j
+#     array[i], array[min_index] = array[min_index], array[i]
 
-print(array)
+# print(array)
 
 # 삽입정렬
-array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+# array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
-for i in range(1, len(array)):
-    for j in range(i, 0, -1):
-        if array[j] < array[j-1]:
-            array[j], array[j-1] = array[j-1], array[j]
-        else:
-            break
-print(array)
+# for i in range(1, len(array)):
+#     for j in range(i, 0, -1):
+#         if array[j] < array[j-1]:
+#             array[j], array[j-1] = array[j-1], array[j]
+#         else:
+#             break
+# print(array)
+
+# 정렬 다음문제
+# array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+# for i in range(len(array)):
+#     min_index = i
+#     for j in range(i+1, len(array)):
+#         if array[min_index] > array[j]:
+#             min_index = j
+#     array[i], array[min_index] = array[min_index], array[i]
+
+# print(array)
