854ac030137c7156584719e97fc5f814ae88af0b
amy3374 <119571479+amy3374@users.noreply.github.com>
2023-06-12 23:48:15 +0900

    퀵 정렬

--- 이코테/정렬.py
+++ 이코테/정렬.py
@@ -21,14 +21,28 @@
 #             break
 # print(array)
 
-# 정렬 다음문제
-# array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+# 퀵 정렬
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
-# for i in range(len(array)):
-#     min_index = i
-#     for j in range(i+1, len(array)):
-#         if array[min_index] > array[j]:
-#             min_index = j
-#     array[i], array[min_index] = array[min_index], array[i]
 
-# print(array)
+def quick_sort(array, start, end):
+    if start >= end:
+        return
+    pivot = start
+    left = start + 1
+    right = end
+    while left <= right:
+        while left <= right and array[left] <= array[pivot]:
+            left += 1
+        while left <= right and array[right] >= array[pivot]:
+            right -= 1
+        if left > right:  # index
+            array[right], array[pivot] = array[pivot], array[right]
+        else:
+            array[left], array[right] = array[right], array[left]
+    quick_sort(array, start, right-1)
+    quick_sort(array, right+1, end)
+
+
+quick_sort(array, 0, len(array)-1)
+print(array)
