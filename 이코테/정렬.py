8c60548db0d115e0e94120c0ecec35b30bb4e097
amy3374 <119571479+amy3374@users.noreply.github.com>
2023-07-04 17:23:47 +0900

    dd

--- 이코테/정렬.py
+++ 이코테/정렬.py
@@ -22,27 +22,26 @@
 # print(array)
 
 # 퀵 정렬
-array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+# array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+# def quick_sort(array, start, end):
+#     if start >= end:
+#         return
+#     pivot = start
+#     left = start + 1
+#     right = end
+#     while left <= right:
+#         while left <= right and array[left] <= array[pivot]:
+#             left += 1
+#         while left <= right and array[right] >= array[pivot]:
+#             right -= 1
+#         if left > right:  # index
+#             array[right], array[pivot] = array[pivot], array[right]
+#         else:
+#             array[left], array[right] = array[right], array[left]
+#     quick_sort(array, start, right-1)
+#     quick_sort(array, right+1, end)
 
 
-def quick_sort(array, start, end):
-    if start >= end:
-        return
-    pivot = start
-    left = start + 1
-    right = end
-    while left <= right:
-        while left <= right and array[left] <= array[pivot]:
-            left += 1
-        while left <= right and array[right] >= array[pivot]:
-            right -= 1
-        if left > right:  # index
-            array[right], array[pivot] = array[pivot], array[right]
-        else:
-            array[left], array[right] = array[right], array[left]
-    quick_sort(array, start, right-1)
-    quick_sort(array, right+1, end)
-
-
-quick_sort(array, 0, len(array)-1)
-print(array)
+# quick_sort(array, 0, len(array)-1)
+# print(array)
